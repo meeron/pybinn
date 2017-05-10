242b87070ebaba556cf9bc3d59c6115a067429d2
Miron Jakubowski <mijakubowski@gmail.com>
2017-05-10 23:11:28 +0200

    Encode/decode int

--- pybinn/pybinn.py
+++ pybinn/pybinn.py
@@ -1,9 +1,19 @@
 import io
+from struct import pack, unpack
 
 # Data Formats
 BINN_OBJECT = b'\xe2'
 
 BINN_STRING = b'\xa0'
+
+BINN_UINT8  = b'\x20'
+BINN_INT8   = b'\x21'
+BINN_UINT16 = b'\x40'
+BINN_INT16  = b'\x41'
+BINN_UINT32 = b'\x60'
+BINN_INT32  = b'\x61'
+BINN_UINT64 = b'\x80'
+BINN_INT64  = b'\x81'
 
 class _Encoder(object):
     def __init__(self):
@@ -17,12 +27,60 @@ class _Encoder(object):
         value_type = type(value)
         if value_type is str:
             self._encode_str(value)
+        if value_type is int:
+            self._encode_int(value)
 
     def _encode_str(self, value):
         size = len(value.encode('utf8'))
         self._buffer.write(BINN_STRING)
         self._buffer.write(self._to_varint(size))
         self._buffer.write(value.encode('utf8') + b'\0')
+
+    def _encode_uint(self, value):
+        # unsigned char (byte)
+        if value < 0x100:
+            self._buffer.write(BINN_UINT8)
+            self._buffer.write(pack('B', value))
+            return
+        # unsigned short
+        if value < 0x10000:
+            self._buffer.write(BINN_UINT16)
+            self._buffer.write(pack('H', value))
+            return
+        # unsigned int
+        if value < 0x100000000:
+            self._buffer.write(BINN_UINT32)
+            self._buffer.write(pack('I', value))
+            return
+        # unsigned long
+        if value < 0x10000000000000000:
+            self._buffer.write(BINN_UINT64)
+            self._buffer.write(pack('L', value))
+            return
+        raise OverflowError("Value to big {}.".format(hex(value)))
+
+    def _encode_int(self, value):
+        if value >= 0:
+            return self._encode_uint(value)
+        # signed char
+        if value >= -0x80:
+            self._buffer.write(BINN_INT8)
+            self._buffer.write(pack('b', value))
+            return
+        # short
+        if value >= -0x8000:
+            self._buffer.write(BINN_INT16)
+            self._buffer.write(pack('h', value))
+            return
+        # int
+        if value >= -0x80000000:
+            self._buffer.write(BINN_INT32)
+            self._buffer.write(pack('i', value))
+            return
+        # long
+        if value >= -0x8000000000000000:
+            self._buffer.write(BINN_INT64)
+            self._buffer.write(pack('l', value))
 
     def _to_varint(self, value):
         if value > 127:
@@ -37,6 +95,22 @@ class _Decoder(object):
         type = self._buffer.read(1)
         if type == BINN_STRING:
             return self._decode_str()
+        if type == BINN_UINT8:
+            return unpack('B', self._buffer.read(1))[0]
+        if type == BINN_INT8:
+            return unpack('b', self._buffer.read(1))[0]
+        if type == BINN_UINT16:
+            return unpack('H', self._buffer.read(2))[0]
+        if type == BINN_INT16:
+            return unpack('h', self._buffer.read(2))[0]
+        if type == BINN_UINT32:
+            return unpack('I', self._buffer.read(4))[0]
+        if type == BINN_INT32:
+            return unpack('i', self._buffer.read(4))[0]
+        if type == BINN_UINT64:
+            return unpack('L', self._buffer.read(8))[0]
+        if type == BINN_INT64:
+            return unpack('l', self._buffer.read(8))[0]
         return None
 
     def _decode_str(self):
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 
 from pybinn import pybinn
 
@@ -6,11 +7,29 @@ class PyBinnEncodeTests(unittest.TestCase):
 
     def test_str(self):
         test_str = "test"
-        self.assertEqual(test_str, pybinn.loads(pybinn.dumps(test_str)))
-        
-    def test_str_long(self):
         test_str_long = ",".join(["test"] * 100)
+        
+        self.assertEqual(test_str, pybinn.loads(pybinn.dumps(test_str)))
         self.assertEqual(test_str_long, pybinn.loads(pybinn.dumps(test_str_long)))
+
+    def test_int(self):
+        test_uchar = 0x100 - 1
+        test_schar = -0x80
+        test_ushort = 0x10000 - 1
+        test_short = -0x8000
+        test_uint = 0x100000000 - 1
+        test_int = -0x80000000
+        test_ulong = 0x10000000000000000 - 1
+        test_long = -0x8000000000000000
+
+        self.assertEqual(test_uchar, pybinn.loads(pybinn.dumps(test_uchar)))
+        self.assertEqual(test_schar, pybinn.loads(pybinn.dumps(test_schar)))
+        self.assertEqual(test_ushort, pybinn.loads(pybinn.dumps(test_ushort)))
+        self.assertEqual(test_short, pybinn.loads(pybinn.dumps(test_short)))
+        self.assertEqual(test_uint, pybinn.loads(pybinn.dumps(test_uint)))
+        self.assertEqual(test_int, pybinn.loads(pybinn.dumps(test_int)))
+        self.assertEqual(test_ulong, pybinn.loads(pybinn.dumps(test_ulong)))
+        self.assertEqual(test_long, pybinn.loads(pybinn.dumps(test_long)))
 
 if __name__ == '__main__':
     unittest.main()
