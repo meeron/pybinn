90cdd7395108e4df4d5b6805bf79ac7bed9197c0
Miron Jakubowski <mijakubowski@gmail.com>
2017-05-14 12:50:38 +0200

    TravisCI support

--- test_pybinn.py
+++ test_pybinn.py
@@ -1,15 +1,14 @@
-import unittest
-import time
+from time import gmtime
 
 import pybinn
 
-class PyBinnEncodeTests(unittest.TestCase):
+class TestPyBinn:
     """PyBinn tests"""
     def test_encode_decode(self):
         """Test encoding and decoding"""
         test = [
             True, False, None,
-            time.gmtime(),
+            gmtime(),
             0, 1, -1, 2, -2, 4, -4, 6, -6,
             0x10, -0x10, 0x20, -0x20, 0x40, -0x40,
             0x80, -0x80, 0x100, -0x100, 0x200, -0x100,
@@ -24,7 +23,4 @@ class PyBinnEncodeTests(unittest.TestCase):
             [1, 2, 3], [], {'name': "Miron", 'age': 32}, {},
             {'a': 1, 'b': 2, 'c': [1, 2, 3]}
         ]
-        self.assertEqual(test, pybinn.loads(pybinn.dumps(test)))
-        
-if __name__ == '__main__':
-    unittest.main()
+        assert test == pybinn.loads(pybinn.dumps(test))
