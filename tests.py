8b458228afea20877dd625fb0bdcb6081bdfec33
Miron Jakubowski <mijakubowski@gmail.com>
2017-05-14 09:32:51 +0200

    Created one test that include all types

--- tests.py
+++ tests.py
@@ -3,69 +3,25 @@ import unittest, sys, time
 from pybinn import pybinn
 
 class PyBinnEncodeTests(unittest.TestCase):
-
-    def test_str(self):
-        test_str = "test"
-        test_str_long = ",".join(["test"] * 100)
+    def test_encode_decode(self):
+        test = [
+        True, False, None,
+        time.gmtime(),
+        0, 1, -1, 2, -2, 4, -4, 6, -6,
+        0x10, -0x10, 0x20, -0x20, 0x40, -0x40,
+        0x80, -0x80, 0x100, -0x100, 0x200, -0x100,
+        0x1000, -0x1000, 0x10000, -0x10000,
+        0x20000, -0x20000, 0x40000,-0x40000,
+        10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
+        10000000000, 100000000000, 1000000000000,
+        -10, -100, -1000, -10000, -100000, -1000000, -10000000, -100000000,
+        -1000000000, -10000000000, -100000000000, -1000000000000,
+        1.1, 0.1, -0.02,
+        "hello", "world", "Hello".encode('utf8'), "World".encode('utf8'),
+        [1,2,3], [], {'name': "Miron", 'age': 32}, {},
+        {'a': 1, 'b': 2, 'c': [1, 2, 3]}]
         
-        self.assertEqual(test_str, pybinn.loads(pybinn.dumps(test_str)))
-        self.assertEqual(test_str_long, pybinn.loads(pybinn.dumps(test_str_long)))
-
-    def test_int(self):
-        test_uchar = 0x100 - 1
-        test_schar = -0x80
-        test_ushort = 0x10000 - 1
-        test_short = -0x8000
-        test_uint = 0x100000000 - 1
-        test_int = -0x80000000
-        test_ulong = 0x10000000000000000 - 1
-        test_long = -0x8000000000000000
-
-        self.assertEqual(test_uchar, pybinn.loads(pybinn.dumps(test_uchar)))
-        self.assertEqual(test_schar, pybinn.loads(pybinn.dumps(test_schar)))
-        self.assertEqual(test_ushort, pybinn.loads(pybinn.dumps(test_ushort)))
-        self.assertEqual(test_short, pybinn.loads(pybinn.dumps(test_short)))
-        self.assertEqual(test_uint, pybinn.loads(pybinn.dumps(test_uint)))
-        self.assertEqual(test_int, pybinn.loads(pybinn.dumps(test_int)))
-        self.assertEqual(test_ulong, pybinn.loads(pybinn.dumps(test_ulong)))
-        self.assertEqual(test_long, pybinn.loads(pybinn.dumps(test_long)))
-
-    def test_bool(self):
-        test_true = True
-        test_false = False
-
-        self.assertEqual(test_true, pybinn.loads(pybinn.dumps(test_true)))
-        self.assertEqual(test_false, pybinn.loads(pybinn.dumps(test_false)))
-
-    def test_None(self):
-        test_None = None
-        
-        self.assertEqual(test_None, pybinn.loads(pybinn.dumps(test_None)))
-
-    def test_float(self):
-        test_float = 9999999.9999999
-
-        self.assertEqual(test_float, pybinn.loads(pybinn.dumps(test_float)))
-
-    def test_bytes(self):
-        test_bytes = "some test string".encode('utf8')
-
-        self.assertEqual(test_bytes, pybinn.loads(pybinn.dumps(test_bytes)))
-
-    def test_time(self):
-        test_time = time.gmtime()
-
-        self.assertEqual(test_time, pybinn.loads(pybinn.dumps(test_time)))
-
-    def test_colletion(self):
-        test_array = [66, "test", [1,2,3,4,5], time.time(), -234234]
-
-        self.assertEqual(test_array, pybinn.loads(pybinn.dumps(test_array)))
-
-    def test_dict(self):
-        test_dict = { 'id': 66, 'name': "test", 'child': { 'hello':"world" }, 'list': [1,2,3] }
-
-        self.assertEqual(test_dict, pybinn.loads(pybinn.dumps(test_dict)))
+        self.assertEqual(test, pybinn.loads(pybinn.dumps(test))) 
         
 if __name__ == '__main__':
     unittest.main()
